cabd25316442dcf230bfa43f941888f91ebcd195
Miraziz <rikhsiboyevmiraziz@gmail.com>
2021-01-20 09:52:22 +0500

    09:48

---
+++ script.js
@@ -0,0 +1,10 @@
+let ism = prompt('Ismingizni kiriting')
+let thisYear = +prompt('Hozirgi yilni kiriting')
+let birthYear = +prompt('Tug\'ilgan yilingizni kiriting')
+let data = 'yoshingiz'
+
+function result(a,b,c,d) {
+    console.log(a,b,c-d);
+}
+
+result(ism,data,thisYear,birthYear);
